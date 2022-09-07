efa2e8a67ddb5e9045e02218b9064e07ae3ebc4b
aurorex <selva_beat@hotmail.com>
2022-09-06 22:13:07 -0500

    create checkbox form

--- public/frontend/src/components/nuevo/desarrollo/inicio/RegistroForm/InicioActividadUno/index.jsx
+++ public/frontend/src/components/nuevo/desarrollo/inicio/RegistroForm/InicioActividadUno/index.jsx
@@ -7,6 +7,7 @@ import Button from 'react-bootstrap/Button';
 import InputForm from '../../../../../commons/forms/InputForm';
 import TextareaForm from '../../../../../commons/forms/TextareaForm';
 import SelectForm from '../../../../../commons/forms/SelectForm';
+import CheckboxForm from '../../../../../commons/forms/CheckboxForm';
 
 const schema = yup.object({
   descripcion: yup.string().required(),
@@ -43,7 +44,50 @@ const InicioActividadUno = () => {
         <TextareaForm name="objetivos" label="Objetivos" register={register} errors={errors}/>
         <TextareaForm name="justificacion" label="Justificación" register={register} errors={errors}/>
         <TextareaForm name="enfoque" label="Enfoque" register={register} errors={errors}/>
-        <Button type="submit" variant="primary">Primary</Button>
+        <Row>
+          <Col xs={12}>
+            <CheckboxForm name="aceptaComponentesMitigación" label={null} register={register} errors={errors}/>
+            <p className="fs-6">Componentes de mitigación</p>
+          </Col>
+          <Col>
+            <SelectForm name="areaMitigacion" label="Área de mitigación" options="option 1" register={register} errors={errors}/>
+          </Col>
+          <Col>
+            <InputForm name="mitigacionEstimada" label="Mitigación estimada" placeholder="Ton CO2Eq" register={register} errors={errors}/>
+          </Col>
+        </Row>
+        <Row>
+          <Col xs={12}>
+            <CheckboxForm name="aceptaComponentesAdaptacion" label={null} register={register} errors={errors}/>
+            <p className="fs-6">Componentes de adaptación</p>
+          </Col>
+          <Col xs={6}>
+            <InputForm name="beneficiarios" label="N° de beneficiarios" placeholder={null} register={register} errors={errors}/>
+          </Col>
+          <Col xs={6}>
+            <InputForm name="poblacion" label="Población" placeholder="%" register={register} errors={errors}/>
+          </Col>
+          <Col xs={6}>
+            <InputForm name="costoTotal" label="Costo total" placeholder="USD" register={register} errors={errors}/>
+          </Col>
+          <Col xs={6}>
+            <InputForm name="duracion" label="Duración" placeholder="Días" register={register} errors={errors}/>
+          </Col>
+        </Row>
+        <Row>
+          <Col xs={12}>
+            <CheckboxForm name="aceptaInstrumentoFinanciero" label={null} register={register} errors={errors}/>
+            <p className="fs-6">Instrumento financiero</p>
+          </Col>
+          <Col xs={12}>
+            <SelectForm name="tipoInstrumentoFinanciero" label="Tipo de instrumento financiero" options="option 1" register={register} errors={errors}/>
+          </Col>
+        </Row>
+        <Row>
+          <div>
+            <Button type="submit" variant="primary">icon</Button>
+          </div>
+        </Row>
       </form>
     );
 }
