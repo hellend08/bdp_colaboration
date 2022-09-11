faf91463698c6dbf5b6c5312360f4524198b1ac8
aurorex <selva_beat@hotmail.com>
2022-09-11 18:25:07 -0500

    modified input form

--- public/frontend/src/components/commons/forms/Input/index.jsx
+++ public/frontend/src/components/commons/forms/Input/index.jsx
@@ -1,18 +1,14 @@
 import Form from 'react-bootstrap/Form';
 import InputGroup from 'react-bootstrap/InputGroup';
 
-const Input = ({name, type, label, placeholder, textAlternative, register, errors}) => {
-
+const Input = ({name, type, label, placeholder, register, errors}) => {
     return (
-        // <Form.Group md="4" controlId="validationCustomUsername">
-        <Form.Group controlId="validationCustomUsername">
-             {/* className="mb-3" */}
-            <Form.Label>{label}</Form.Label>
-            <InputGroup hasValidation>
-                <Form.Control {...register(name)} type={type} placeholder={textAlternative} />
+        <Form.Group md="4" controlId="validationCustomUsername">
+        <Form.Label>{label}</Form.Label>
+            <InputGroup className="mb-3">
+                <Form.Control {...register(name)} type={type} />
                 { placeholder && <InputGroup.Text id="inputGroupPrepend">{placeholder}</InputGroup.Text>}
-                {/* <p>{errors}</p>   */}
-                {/* <p className="text-danger text-small d-block mb-2">
+            {/* <p className="text-danger text-small d-block mb-2">
                     {errors?.name?.message}
                 </p> */}
             </InputGroup>
